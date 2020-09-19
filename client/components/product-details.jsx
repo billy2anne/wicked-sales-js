13403d01b5426f23be03499ca3e1d501b861ba92
billy2anne <kimroque@gmail.com>
2020-09-19 20:05:44 +0000

    completed view product details frontend

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -27,10 +27,20 @@ export default class ProductDetails extends React.Component {
       return null;
     } else {
       return (
-        <div onClick={this.setView}>
-          productInfo goes here
-        </div>
-
+        <>
+          <div onClick={this.setView}>
+            &lt;Back to catalog
+          </div>
+          <div className="prodDetailsContainer col-12">
+            <img src={this.state.product.image} className="detailsImage col-6" />
+            <div className="detailsContentcontainer col-6">
+              <h1 className="nameProductDetails">{this.state.product.name}</h1>
+              <div className="price">${this.state.product.price}</div>
+              <div className="description">{this.state.product.shortDescription}</div>
+            </div>
+          </div>
+          <div className="longDescription">{this.state.product.longDescription}</div>
+        </>
       );
     }
   }
